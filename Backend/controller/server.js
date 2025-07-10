0f9927216fbf3f7a788f462626b677b9c7e63269
suneelgiree <suneel.giri641@gmail.com>
2025-07-10 22:01:37 +0545

    Add server.js, package-lock.json, and uploads folder for new backend features

---
+++ Backend/controller/server.js
@@ -0,0 +1,51 @@
+
+require('dotenv').config();
+
+const express = require("express");
+const app = express();
+const handleServerError = require("./middleware/handleServerError.js");
+require("./config/database.js");
+const authRoutes = require("./routes/auth.js");
+
+const bookingRoutes = require("./routes/booking.js");
+
+const cricksalRoutes = require("./routes/cricksal.js")
+const AllUserRoutes = require("./routes/AdminPanel.js")
+
+const PaymentRoutes = require("./routes/PaymentRoutes.js")
+
+const profilesRoutes = require("./routes/profiles.js")
+
+const reviewRoutes = require("./routes/review.js")
+
+
+
+const fileUpload = require("express-fileupload")
+const cors = require("cors");
+const { startBookingStatusUpdater } = require('./controller/booking.js');
+
+
+app.use(express.json());
+app.use(cors());
+app.use(fileUpload())
+app.use(express.static('uploads'))//http://localhost:8000/mm.jpg yo dekhina ko lagi
+// public path
+
+app.use(authRoutes);
+app.use(cricksalRoutes);
+app.use(bookingRoutes)
+app.use(AllUserRoutes);
+app.use(PaymentRoutes) 
+app.use(profilesRoutes);
+app.use(reviewRoutes);
+
+startBookingStatusUpdater();
+
+app.use((req, res) => {
+  res.status(404).send({ msg: "Resource not found" });
+});
+app.use(handleServerError);
+
+app.listen(8000, () => {
+  console.log("server started....");
+});
