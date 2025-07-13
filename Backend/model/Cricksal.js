702d0062efbbad1a668868b75dea32e24da9085a
Birat <itsbirataryal+github@gmail.com>
2025-07-13 17:29:10 +0545

    Add Cricksal, Booking, and Payment models to the backend

---
+++ Backend/model/Cricksal.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const CricksalSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  pricePerHour: {
+    type: Number,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  location:{
+    type: String,
+  },
+  
+  images: {
+    type: [String], // Supports multiple image paths
+    required: true,
+    validate: {
+      validator: function (value) {
+        return value.length >= 3; // Ensures at least 3 images
+      },
+      message: "At least 3 images are required.",
+    },
+  },
+  createdBy: {
+    ref: "User",
+    type: ObjectId,
+    required: true,
+  },
+
+
+});
+
+const Cricksal = mongoose.model("Cricksal", CricksalSchema);
+
+module.exports = Cricksal;
