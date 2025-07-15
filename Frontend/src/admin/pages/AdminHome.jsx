950762b797a8683c104272039adb46d20f26efed
Birat <itsbirataryal+github@gmail.com>
2025-07-15 12:36:40 +0545

    Add updateBooking function and route for admin booking updates; remove AllPayment component

--- Frontend/src/admin/pages/AdminHome.jsx
+++ Frontend/src/admin/pages/AdminHome.jsx
@@ -1,4 +1,3 @@
-
 import AdminSidebar from '../components/AdminSidebar'
 import { Route, Routes } from 'react-router-dom';
 import AdminDashboard from '../components/AdminDashboard';
@@ -10,13 +9,14 @@ import AllUsersForAdmin from './AllUser';
 
 const AdminHome = () => {
   return (
-    <div className="flex h-screen bg-gradient-to-br from-dark-blue to-accent-blue">
+    <div className="bg-gradient-to-b from-gray-800 to-gray-900 text-gray-300 min-h-screen p-6">
             {/* Sidebar */}
             <div className="w-64">
                 <AdminSidebar />
             </div>
 
             <div className="flex-1 overflow-auto">
+                {/* <h1 className="text-3xl font-bold text-gradient bg-gradient-to-r from-blue-400 to-green-400 bg-clip-text text-transparent mb-6">Admin Dashboard</h1> */}
                 <Routes>
                     <Route path="/" element={<AdminDashboard/>} />
                     <Route path="/arenas" element={<ManageCricksal />} />
